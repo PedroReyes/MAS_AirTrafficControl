6e936e03e4164b2cdbfcbbcfd7528803f3890750
FJavierMoreno <fcojaviermorenobarea@uma.es>
2016-03-01 18:22:42 +0100

    Clase Pista

---
+++ Auxiliar/Pista.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Auxiliar;
+
+/**
+ *
+ * @author j_sto
+ */
+public class Pista {
+
+    private int coordenadaX;
+    private int coordenadaY;
+
+    private int entradaX;
+    private int entradaY;
+
+    private int stepsAterrizaje;
+
+    public Pista(int coorX, int coorY, int entX, int entY, int time) {
+        this.coordenadaX = coorX;
+        this.coordenadaY = coorY;
+        this.entradaX = entX;
+        this.entradaY = entY;
+        this.stepsAterrizaje = time;
+    }
+    
+    public void setCoordenadaX(int coordenadaX){
+        this.coordenadaX = coordenadaX;
+    }
+    
+    public int getCoordenadaX(){
+        return this.coordenadaX;
+    }
+    
+    public void setCoordenadaY(int coordenadaY){
+        this.coordenadaY = coordenadaY;
+    }
+    
+    public int getCoordenadaY(){
+        return this.coordenadaY;
+    }
+    
+    public void setEntradaX(int entradaX){
+        this.entradaX = entradaX;
+    }
+    
+    public int getEntradaX(){
+        return this.entradaX;
+    }
+    
+    public void setEntradaY(int entradaY){
+        this.entradaY = entradaY;
+    }
+    
+    public int getEntradaY(){
+        return this.entradaY;
+    }
+    
+    public void setStepsAterrizaje(int stepsAterrizaje){
+        this.stepsAterrizaje = stepsAterrizaje;
+    }
+    
+    public int getStepsAterrizaje(){
+        return this.stepsAterrizaje;
+    }
+}
