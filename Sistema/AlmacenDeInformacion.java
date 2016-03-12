11a747f00f1919d2b69237253fc896010fed1043
FJavierMoreno <fcojaviermorenobarea@uma.es>
2016-03-12 14:57:04 +0100

    Añadido comportamiento del ADI
    El Almacen de Informacion ya tiene un comportamiento. Lo que hace es
    simple y llanamente recibir mensajes. Mira que dice el mensaje y lo
    hace, acto tras el cual informa a los agentes pertinentes. Para ello
    hace falta añadirle al mensaje que se le manda al almacen, un ReplyTo.
    De este modo no hemos de distinguir si el mensaje nos lo manda ATC o un
    Avion, salvo que sea de Modificacion.

--- Sistema/AlmacenDeInformacion.java
+++ Sistema/AlmacenDeInformacion.java
@@ -5,94 +5,101 @@
  */
 package Sistema;
 
+import Auxiliar.Vector;
+import jade.core.AID;
 import jade.core.Agent;
-import jade.core.behaviours.SimpleBehaviour;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * @author Javier Moreno
  */
-public class AlmacenDeInformacion extends Agent{
-    private List<Avion> aviones;
-    private List<Boolean> modificado;
-    
+public class AlmacenDeInformacion extends Agent {
+
+    private HashMap<String, Avion> aviones;
+
     // =========================================================================
     // AGENT
     // =========================================================================
     @Override
     public void setup() {
-        SimpleBehaviour almInformacion = new SimpleBehaviour(this) {
-            boolean finished = false;
-            int state = 0;
-
+        CyclicBehaviour almInformacion = new CyclicBehaviour(this) {
             @Override
             public void action() {
                 ACLMessage msg = receive();
                 if (msg != null) {
-                    System.out.println("Recibido nuevo mensaje"+msg.getContent());
-                    
-                    switch (msg.getContent()) {
-                        case "ADD":
-                            actualizarInformacion();
-                            informar("ATC/Aviones");
-                            informar("Logger");
-                            break;
-                        case "REM":
-                            actualizarInformacion();
-                            informar("ATC/Aviones");
-                            informar("Logger");
-                            break;
-                        case "MOD":
-                            actualizarInformacion();
-                            informar("ATC/Aviones");
-                            informar("Logger");
-                            break;
-                        default:
-                            break;
+                    System.out.println("Recibido nuevo mensaje" + msg.getContent());
+                    actualizarInformacion(msg.getContent());
+                    for (Iterator iter = msg.getAllReplyTo(); iter.hasNext();) {
+                        System.out.println(iter.next());
+                        informar((AID) iter.next(), msg.getContent());
                     }
                 } else {
-                    System.out.println("No recibo nada "+state);
-                    //block();
+                    System.out.println("No recibo nada");
+                    block();
                 }
-                state++;
-            }
-
-            @Override
-            public boolean done() {
-                return finished;
             }
         };
 
         addBehaviour(almInformacion);
     }
-    
+
+    // =========================================================================
+    // AUXILIARY METHODS
+    // =========================================================================
+    public void actualizarInformacion(String content) {
+        String words[] = content.split(" ");
+        switch (words[1]) {
+            case "ADD":
+                aviones.put(words[2], new Avion(words[2], new Vector(Integer.parseInt(words[3]), Integer.parseInt(words[4]), 0), Double.parseDouble(words[5]), Double.parseDouble(words[6])));
+                break;
+            case "REM":
+                aviones.remove(words[2]);
+                break;
+            case "MOD":
+                Avion aux = aviones.get(words[2]);
+                if(words.length == 4){ //Mensaje de ATC
+                    aux.setVectorDirector(new Vector(Integer.parseInt(words[3]), Integer.parseInt(words[4]), 0));
+                } else { //Mensaje de Avion
+                    aux.setPosicionActual(new Vector(Integer.parseInt(words[3]), Integer.parseInt(words[4]), 0));
+                    aux.setCombustibleActual(Double.parseDouble(words[5]));
+                    aux.setCombustibleXStep(Double.parseDouble(words[6]));
+                }
+                aviones.replace(words[2], aux);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void informar(AID agent, String content) {
+        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+        msg.setContent(content);
+        msg.addReceiver(agent);
+        msg.addReceiver(new AID("gph", AID.ISLOCALNAME));
+        msg.addReceiver(new AID("log", AID.ISLOCALNAME));
+        send(msg);
+    }
+
     // =========================================================================
     // ToString
     // =========================================================================
     @Override
-    public String toString(){
-      return "AlmacenDeInformacion: ["+
-           " "+"]\n";
+    public String toString() {
+        return "AlmacenDeInformacion: ["
+                + aviones.toString() + "]\n";
     }
-    
+
     // =========================================================================
     // GETTERS & SETTERS
     // =========================================================================
-    public void setAviones(List<Avion> avion) {
+    public void setAviones(HashMap<String, Avion> avion) {
         this.aviones = avion;
     }
-    
-    public List<Avion> getAviones() {
+
+    public HashMap<String, Avion> getAviones() {
         return this.aviones;
-    }
-    
-    public void actualizarInformacion(){
-        
-    }
-    
-    public void informar(String agente){
-        
     }
 }
