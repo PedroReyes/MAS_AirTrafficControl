299ec01d2b7db64f30e910ba405d61a4faa5c52d
PedroReyes <eagles.azul@hotmail.com>
2016-03-16 21:11:58 +0100

    logger guarda en fichero

--- Output/Logger.java
+++ Output/Logger.java
@@ -5,42 +5,47 @@
  */
 package Output;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
-import java.io.FileWriter;
-import java.io.PrintWriter;
 
 /**
  * @author Javier Moreno
  */
 public class Logger extends Agent {
 
-    private FileWriter fichero;
-    private PrintWriter pw;
+	static String info = "";
+	static String path = System.getProperty("os.name").equalsIgnoreCase("Mac OS X")
+			? "/Users/pedro/Dropbox/Vida_laboral/Master/ProgramacionSistemasMultiagentes/TrabajoSegundaParte/MAS_AirTrafficControl/Output/log.txt"
+			: "javi";
 
-    @Override
-    public void setup() {
-        try {
-            fichero = new FileWriter("log.txt");
-            pw = new PrintWriter(fichero);
-        } catch (Exception e) {
-        }
+	@Override
+	public void setup() {
 
-        MessageTemplate mAlmacenDInf = MessageTemplate.MatchSender(new AID("adi", AID.ISLOCALNAME));
+		MessageTemplate mAlmacenDInf = MessageTemplate.MatchSender(new AID("adi", AID.ISLOCALNAME));
 
-        addBehaviour(new CyclicBehaviour(this) {
-            @Override
-            public void action() {
-                ACLMessage msg = myAgent.receive(mAlmacenDInf);
-                if (msg != null) {
-                    pw.println(msg.getContent());
-                } else {
-                    block();
-                }
-            }
-        });
-    }
+		addBehaviour(new CyclicBehaviour(this) {
+			@Override
+			public void action() {
+				ACLMessage msg = myAgent.receive(mAlmacenDInf);
+				if (msg != null) {
+					info = info + msg.getContent();
+					try {
+						Files.write(Paths.get(Logger.path), info.getBytes());
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} else {
+					block();
+				}
+			}
+		});
+	}
 }
